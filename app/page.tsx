0303577ce78c43b8bdfc34f13f12b3f9cb85131a
takezawahiroshi <takezawahiroshi@gmail.com>
2024-01-03 23:18:39 +0900

    init

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import NotesList from './components/notes-list'
+import TimerCounter from './components/timer-counter'
+import { Suspense } from 'react'
+import Spinner from './components/spinner'
+import RefreshBtn from './components/refresh-btn'
+
+export default function Page() {
+  return (
+    <main>
+      <div className="m-10 text-center">
+        <p>Hello World</p>
+        <Suspense fallback={<Spinner color="border-green-500" />}>
+          <NotesList />
+        </Suspense>
+        <TimerCounter />
+        <RefreshBtn />
+      </div>
+    </main>
+  )
+}
